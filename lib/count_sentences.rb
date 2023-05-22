a152a7cc9f9b7021add8398d0652207eefdabc2e
Benah0987 <wanyoikebenayah77@gmail.com>
2023-05-22 14:55:05 +0300

    using the self, end_with and counting sentences

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
-  end
+    # the end_with? method checks if it ends with the specified mark
+    self.end_with?(".")
+  end 
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    #split method splits the string 
+    # removes any empty elemen from the resulting array 
+    self.split(/[.?!]/).reject(&:empty?).count
   end
 end
